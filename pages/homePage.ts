c5d7cc355d92473a69cce8e714825aaf8af7f85b
Shindeas4 <aditya.231136@gmail.com>
2025-03-11 22:51:30 +0530

    added faker dependency to generate test data and added home page locators

---
+++ pages/homePage.ts
@@ -0,0 +1,18 @@
+import {Page,Locator } from "@playwright/test"
+
+
+export class HomePage {
+    
+    readonly page :Page ;
+    readonly bannerTxt : Locator ;
+
+    constructor(page :Page){
+        this.page = page ;
+       
+
+    }
+
+    async 
+
+
+}
